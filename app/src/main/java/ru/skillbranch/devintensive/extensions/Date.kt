2c37cbdeca356795b63c2c225a0f2e2e6fcde320
victor <vtsurul@gmai.com>
2019-07-09 09:45:56 +0700

    finish hometask_2

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -40,7 +40,7 @@ fun Date.humanizeDiff(date: Date = Date()): String {
             0.sec, 1.sec -> "только что"
             in 2.sec..45.sec -> "несколько секунд назад"
             in 46.sec..75.sec -> "минуту назад"
-            in 76.sec..45.min -> "${TimeUnits.MINUTE.plural(diff/MINUTE)} назад"
+            in 76.sec..45.min -> "${TimeUnits.MINUTE.plural(diff/ MINUTE)} назад"
             in 45.min + 1..75.min -> "час назад"
             in 75.min + 1..22.hour -> "${TimeUnits.HOUR.plural(diff/HOUR)} назад"
             in 22.hour + 1..26.hour -> "день назад"
@@ -50,15 +50,15 @@ fun Date.humanizeDiff(date: Date = Date()): String {
 
     } else {
 
-        when (- diff) {
+        when (-1 * diff) {
             0.sec, 1.sec -> "сейчас"
             in 2.sec .. 45.sec -> "через несколько секунд"
             in 46.sec .. 75.sec -> "через минуту"
-            in 76.sec .. 45.min -> "через ${TimeUnits.MINUTE.plural(diff/ MINUTE)}"
+            in 76.sec .. 45.min -> "через ${TimeUnits.MINUTE.plural(-diff/ MINUTE)}"
             in 45.min + 1 .. 75.min -> "через час"
-            in 75.min + 1 .. 22.hour -> "через ${TimeUnits.HOUR.plural(diff/HOUR)}"
+            in 75.min + 1 .. 22.hour -> "через ${TimeUnits.HOUR.plural(-diff/HOUR)}"
             in 22.hour + 1 .. 26.hour -> "через день"
-            in 26.hour + 1 .. 360.day -> "через ${TimeUnits.DAY.plural(diff/DAY)}"
+            in 26.hour + 1 .. 360.day -> "через ${TimeUnits.DAY.plural(-diff/DAY)}"
             else -> "более чем через год"
         }
     }
@@ -71,16 +71,16 @@ enum class TimeUnits
     fun plural(value: Long) :String {
 
         return "$value ${pluralMap()[
-                when (value.toInt()) {
-                    1 -> "один"
-                    2, 3, 4 -> "мало"
+                when (value) {
+                    1.toLong() -> "один"
+                    2.toLong(), 3.toLong(), 4.toLong() -> "мало"
                     else -> "много"
                 }]}"
     }
 
     private fun pluralMap(): Map<String, String> = when(this) {
         SECOND -> mapOf("один" to "секунда", "мало" to "секунды", "много" to "секунд")
-        MINUTE -> mapOf("один" to "минута", "мало" to "минуты", "много" to "минут")
+        MINUTE -> mapOf("один" to "минуту", "мало" to "минуты", "много" to "минут")
         HOUR -> mapOf("один" to "час", "мало" to "часа", "много" to "часов")
         DAY -> mapOf("один" to "день", "мало" to "дня", "много" to "дней")
     }
