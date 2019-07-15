6e32d13a5b7432ea551361e99b482c2733549a30
victor <vtsurul@gmai.com>
2019-07-15 19:47:18 +0700

    finish hometask_2

--- app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
@@ -12,6 +12,6 @@ class ImageMessage(
     var image: String?
 ) : BaseMessage(id, from, chat, isIncoming, date) {
 
-       override fun formatMessage(): String = "id:$id ${from?.firstName}" +
+       override fun formatMessage(): String = "${from?.firstName} " +
                 "${if (isIncoming) "получил" else "отправил"} изображение \"$image\" ${date.humanizeDiff()}"
 }
--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -12,7 +12,7 @@ class TextMessage(
     var text: String?
 ) : BaseMessage(id, from, chat, isIncoming, date) {
 
-    override fun formatMessage(): String = "id:$id ${from?.firstName}" +
+    override fun formatMessage(): String = "${from?.firstName} " +
             "${if (isIncoming) "получил" else "отправил"} сообщение \"$text\" ${date.humanizeDiff()}"
 }
 
